202babe33eda7281f3f6334d4fd66475dbcb9d0d
James Sundra <james@matterport.com>
2015-02-15 22:04:34 -0500

    Spawning, basic class framework, 2D movement.

---
+++ CoralReef/Assets/Scripts/FishController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishController : MonoBehaviour {
+
+	protected FishGenetics genetics;
+
+
+	protected Vector3 destination = Vector3.one;
+	protected float dstHeading;
+
+	protected float turnSpeedMax = 25;
+
+	protected bool init;
+
+	public void Init(int typeIndex){
+		if(init) return;
+
+		genetics = FishGenetics.Create(typeIndex);
+
+		init = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(transform.position, destination) <= 0.5f){
+			Debug.Log("Redirecting!");
+			destination = transform.parent.position + FishSpawner.GetRandomSpawnVector3(); //Temporary!
+		}
+
+		//if move
+		Vector3 dstCross = Vector3.Cross(transform.forward, destination - transform.position);
+		int yawDir = dstCross.y > 0 ? 1 : -1;
+		//int pitchDir = dstCross.x > 0 ? 1 : -1;
+
+		transform.Rotate(0, yawDir * turnSpeedMax * Time.deltaTime, 0);
+
+		transform.Translate(Vector3.forward * Time.deltaTime);
+	}
+}
+
+
+public struct FishGenetics {
+	
+	public enum Fish_Hierarchy { Goldfish, Bass, Salmon, Tuna };
+	
+	public static float[] Fish_Sight_Breadth = { 10, 20, 30, 40 };
+	public static float[] Fish_Sight_Depth = { 10, 20, 30, 40 };
+
+	public Fish_Hierarchy fishType;
+	public float sight;
+	
+	
+	public static FishGenetics Create(int typeIndex){
+
+		FishGenetics data = new FishGenetics();
+
+		data.fishType = (Fish_Hierarchy)typeIndex;
+		data.sight = Fish_Sight_Breadth[typeIndex];
+
+		return data;
+	}
+}
---
+++ CoralReef/Assets/Scripts/FishSpawner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishSpawner : MonoBehaviour {
+
+	private static FishSpawner instance;
+
+	public GameObject[] Fish;
+	public int[] FishToSpawn;
+
+	public Vector2 spawnHeight;
+	public Vector2 spawnRadius;
+
+	// Use this for initialization
+	private void Start () {
+		instance = this;
+
+		if(Fish.Length != FishToSpawn.Length){
+			Debug.LogError("Uneven # of fish and spawn numbers!");
+			return;
+		}
+
+		StartCoroutine(SpawnCoroutine());
+	}
+	
+	private IEnumerator SpawnCoroutine(){
+		for(int i = 0; i < FishToSpawn.Length; i++){
+			for(int j = 0; j < FishToSpawn[i]; j++){
+				GameObject newFish = ((GameObject)(GameObject.Instantiate(Fish[i], transform.position + GetRandomSpawnVector3(), GetRandomSpawnQuaternion())));
+				newFish.transform.parent = transform;
+				newFish.transform.localScale = Vector3.one;
+
+				newFish.GetComponent<FishController>().Init(i);
+				yield return new WaitForEndOfFrame();
+			}
+		}
+	}
+
+	public static Vector3 GetRandomSpawnVector3(){
+		return new Vector3(Random.Range(instance.spawnRadius.x, instance.spawnRadius.y),
+		                   Random.Range(instance.spawnHeight.x, instance.spawnHeight.y),
+		                   Random.Range(instance.spawnRadius.x, instance.spawnRadius.y));
+	}
+
+	private Quaternion GetRandomSpawnQuaternion(){
+		return Quaternion.Euler(new Vector3(0, Random.Range(0, 359), 0));
+	}
+}
