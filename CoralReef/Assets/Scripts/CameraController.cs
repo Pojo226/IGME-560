371d434d27fcfc1264d10335a4e5845556493b0e
unknown <jps1545@GDD-7.main.ad.rit.edu>
2015-05-13 09:40:17 -0400

    Linear camera movement

---
+++ CoralReef/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public float speed;
+
+    private void Update(){
+        if(Input.GetKey(KeyCode.W)){
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.S)){
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.A)){
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.Space)){
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.LeftControl)){
+            transform.position -= transform.up * speed * Time.deltaTime;
+        }
+    }
+
+}
